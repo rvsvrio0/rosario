4205822a1aa83d128ea270a8ffb20054a32dc961
Rosario Martina <marocalo2007@gmail.com>
2026-07-16 17:09:44 -0400

    fix up

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+const song = document.getElementById('homesong');
 const discBtn = document.querySelector('.disc-container');
+const discImg = document.querySelector('.discimage');
 
-discBtn.addEventListener('dblclick', function() {
- 
+if (discBtn && song && discImg) {
+  discBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    if (song.paused) {
+      song.play();
+      discImg.style.animationPlayState = 'running';
+    } else {
+      song.pause();
+      discImg.style.animationPlayState = 'paused';
+    }
+  });
+
+  discBtn.addEventListener('dblclick', function() {
     const link = document.createElement('a');
-    link.href = 'images01/Bella_Kali.mp3'; 
+    link.href = 'images01/Bella_Kali.mp3';
     link.download = 'Bella_Kali.mp3';
     link.click();
-
-  
-    console.log("Download started :3");
-    
-});
+    console.log('Download started :3');
+  });
+}
